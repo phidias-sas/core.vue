341a79de5cd794f0547ef1bf56bdc4c9eede8dc8
Santiago Cortes <santiago.cortes@phidias.com.co>
2016-12-06 17:37:45 +0100

    dummy state changes

--- src/states/Santiago.vue
+++ src/states/Santiago.vue
@@ -1,45 +1,46 @@
 <template>
-    <div id="santiago">
-        <h1>HOLA MUNDO</h1>
-        <span>{{ titulo }}</span>
-        <button @click="cargarDatos()">Cargar datos</button>
-        <ul>
-            <li v-for="persona in personas">{{ persona.firstName }}</li>
-        </ul>
-    </div>
+	<div id="santiago">
+		<h1 @click="cambiarTitulo()">HOLA MUNDO</h1>
+		<span>{{ titulo }}</span>
+		<button @click="cargarDatos()">Cargar datos</button>
+		<ul>
+			<li v-for="persona in personas">{{ persona.firstName }}</li>
+		</ul>
+	</div>
 </template>
 
 <script>
 export default {
+	data() {
+		return {
+			titulo: 'hola este es el titulo',
+			personas: []
+		}
+	},
 
-    data() {
-        return {
-            titulo: 'hola este es el titulo',
-            personas: []
-        }
-    },
+	methods: {
+		cambiarTitulo() {
+			this.titulo = 'el titulo ha cambiado';
+		},
 
-    methods: {
-        cambiarTitulo() {
-            this.titulo = 'el titulo ha cambiado';
-        },
+		cargarDatos() {
+			fetch('https://phidias.api.phidias.co/people')
+				.then(response => response.json())
+				.then(datos => {
+					this.personas = datos;
+				});
+		}
+	},
 
-        cargarDatos() {
-
-            fetch('https://phidias.api.phidias.co/people')
-                .then(response => response.json())
-                .then(datos => {
-                    this.personas = datos;
-                });
-        }
-    }
-
+	mounted() {
+		this.cargarDatos();
+	}
 }
 </script>
 
 <style lang="scss">
 #santiago {
-    border: 3px solid red;
-    padding: 20px;
+	border: 2px solid red;
+	padding: 20px;
 }
 </style>
