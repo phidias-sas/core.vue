79e8aba97072be34b4e87ac99f7c29cf2c73c4ca
l-beltran <l.beltran@live.com>
2016-12-09 08:59:11 -0500

    added dummy filters pannel

--- src/components/Phi/FullCalendar.vue
+++ src/components/Phi/FullCalendar.vue
@@ -1,25 +1,63 @@
 <template>
-	<ons-page>
-		<ons-toolbar>
-			<div class="left">
-				<ons-toolbar-button data-action="calendar-back">
-					<ons-icon icon="fa-chevron-left"></ons-icon>
-				</ons-toolbar-button>
-				<ons-toolbar-button data-action="calendar-forward">
-					<ons-icon icon="fa-chevron-right"></ons-icon>
-				</ons-toolbar-button>
-			</div>
-			<div class="center">
-				<ons-toolbar-button data-action="calendar-today">Hoy</ons-toolbar-button>
-			</div>
-			<div class="right">
-				<ons-toolbar-button data-action="calendar-month">Mes</ons-toolbar-button>
-				<ons-toolbar-button data-action="calendar-week">Sem</ons-toolbar-button>
-				<ons-toolbar-button data-action="calendar-day">Día</ons-toolbar-button>
-			</div>
-		</ons-toolbar>
-		<div class="calendar-container" :person="person"></div>
-	</ons-page>
+	<ons-splitter>
+  		<ons-splitter-side data-pannel="calendar-filters" side="right" width="85vw" collapse swipeable>
+			<ons-page>
+				<ons-list>
+					<ons-list-item>
+						<label class="left">
+							<ons-input type="checkbox" input-id="check-1"></ons-input>
+						</label>
+						<label for="check-1" class="center">
+							Reuniones
+						</label>
+					</ons-list-item>
+					<ons-list-item>
+						<label class="left">
+							<ons-input type="checkbox" input-id="check-2"></ons-input>
+						</label>
+						<label for="check-2" class="center">
+							Tareas
+						</label>
+					</ons-list-item>
+					<ons-list-item>
+						<label class="left">
+							<ons-input type="checkbox" input-id="check-3"></ons-input>
+						</label>
+						<label for="check-3" class="center">
+							Cumpleaños
+						</label>
+					</ons-list-item>
+				</ons-list>
+			</ons-page>
+  		</ons-splitter-side>
+		<ons-splitter-content>
+			<ons-page>
+				<ons-toolbar>
+					<div class="left">
+						<ons-toolbar-button data-action="calendar-back">
+							<ons-icon icon="fa-chevron-left"></ons-icon>
+						</ons-toolbar-button>
+						<ons-toolbar-button data-action="calendar-forward">
+							<ons-icon icon="fa-chevron-right"></ons-icon>
+						</ons-toolbar-button>
+					</div>
+					<div class="center">
+						<ons-toolbar-button data-action="calendar-today">Hoy</ons-toolbar-button>
+					</div>
+					<div class="right">
+						<ons-toolbar-button data-action="calendar-month">M</ons-toolbar-button>
+						<ons-toolbar-button data-action="calendar-week">S</ons-toolbar-button>
+						<ons-toolbar-button data-action="calendar-day">D</ons-toolbar-button>
+						<ons-toolbar-button data-action="filters-pannel">
+							<ons-icon icon="fa-filter"></ons-icon>
+						</ons-toolbar-button>
+					</div>
+				</ons-toolbar>
+				<div class="calendar-container" :person="person"></div>
+			</ons-page>
+  		</ons-splitter-content>
+	</ons-splitter>
+	
 </template>
 
 <script>
@@ -79,6 +117,7 @@ export default {
 		let monthButton = this.$el.querySelector("ons-toolbar-button[data-action='calendar-month']");
 		let weekButton = this.$el.querySelector("ons-toolbar-button[data-action='calendar-week']");
 		let dayButton = this.$el.querySelector("ons-toolbar-button[data-action='calendar-day']");
+		let filtersButton = this.$el.querySelector("ons-toolbar-button[data-action='filters-pannel']");
 
 		backButton.addEventListener("click", () => $(calenndarContainer).fullCalendar('prev'));
 		forwardButton.addEventListener("click", () => $(calenndarContainer).fullCalendar('next'));
@@ -86,6 +125,8 @@ export default {
 		monthButton.addEventListener("click", () => $(calenndarContainer).fullCalendar('changeView', 'month'));
 		weekButton.addEventListener("click", () => $(calenndarContainer).fullCalendar('changeView', 'agendaWeek'));
 		dayButton.addEventListener("click", () => $(calenndarContainer).fullCalendar('changeView', 'agendaDay'));
+
+		filtersButton.addEventListener("click", () => this.$el.querySelector("ons-splitter-side[data-pannel='calendar-filters']").open());
 
 		let title = this.$el.querySelector("div.fc-toolbar.fc-header-toolbar > div.fc-center > h2");
 		title.style.fontSize = "20px";
