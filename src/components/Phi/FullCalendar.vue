d4d4c1b8e5654e95ef3b77bf9c8d59f1b67186ad
l-beltran <l.beltran@live.com>
2016-12-13 00:36:46 -0500

    even more visual tuning

--- src/components/Phi/FullCalendar.vue
+++ src/components/Phi/FullCalendar.vue
@@ -3,26 +3,26 @@
 		<ons-page>
 			<ons-toolbar>
 				<div class="left">
-					<ons-toolbar-button data-action="calendar-today">
-						<ons-icon icon="fa-calendar-check-o"></ons-icon>
-					</ons-toolbar-button>
-				</div>
-				<div class="center">
-					<ons-toolbar-button data-action="calendar-views"></ons-toolbar-button>
-				</div>
-				<div class="right">
 					<ons-toolbar-button data-action="filters-pannel">
 						<ons-icon icon="fa-filter"></ons-icon>
 					</ons-toolbar-button>
 				</div>
+				<div class="center" data-action="toolbar-title"></div>
+				<div class="right">
+					<ons-toolbar-button data-action="calendar-today">
+						<ons-icon icon="fa-calendar-check-o"></ons-icon>
+					</ons-toolbar-button>
+					<ons-toolbar-button data-action="calendar-views">
+						<ons-icon icon="fa-ellipsis-v"></ons-icon>
+					</ons-toolbar-button>
+				</div>
 			</ons-toolbar>
-
+			<ons-progress-bar data-action="events-loading" v-show="loadingEvents" indeterminate></ons-progress-bar>
 			<div class="calendar-container" :person="person"></div>
 		</ons-page>
 
 		<ons-popover direction="down" data-action="calendar-views" cancelable>
 			<ons-list>
-				<ons-list-header>Vista</ons-list-header>
 				<ons-list-item tappable v-for="calendarDef in calendarViews">
 					<label class="left">
 						<ons-input name="available-calendar-views" type="radio" :input-id="calendarDef.name"></ons-input>
@@ -36,7 +36,6 @@
 
 		<ons-popover direction="down" data-action="calendar-filters" cancelable>
 			<ons-list>
-				<ons-list-header>Filtros</ons-list-header>
 				<ons-list-item tappable>
 					<label class="left">
 						<ons-input name="reuniones" type="checkbox" input-id="reuniones"></ons-input>
@@ -88,7 +87,7 @@ export default {
 				height: 'parent',
 				header: {
 					left: false,
-					center: 'title',
+					center: false,
 					right: false
 				},
 
@@ -106,6 +105,21 @@ export default {
 
 				eventClick: function(thisEvent) {
 					that.$router.push({ name: 'read', params: { threadId: thisEvent.post.thread }});
+				},
+
+				viewRender: function(view, element){
+					let toolbarTitle = that.$el.querySelector("div[data-action='toolbar-title']");
+					toolbarTitle.innerHTML = view.title;
+				},
+
+				loading: function(isLoading, view){
+					if(isLoading){
+						that.loadingEvents = isLoading;
+					}
+				},
+
+				eventAfterAllRender: function(view){
+					that.loadingEvents = false;
 				}
 			},
 
@@ -120,77 +134,75 @@ export default {
 				{name: "listWeek", "es": "lista semanal"}, 
 				{name: "listDay", "es": "lista diaria"}
 			],
-			currentView: null
+			currentView: null,
+			calendarContainer: null,
+			loadingEvents: false
 		}
 	},
 
 	watch: {
-		currentView: function (val) {
-			let viewListButton = this.$el.querySelector("ons-toolbar-button[data-action='calendar-views']");
-
-			this.calendarViews.map(viewDef => {
-				if(viewDef.name == this.currentView){
-					viewListButton.innerHTML = viewDef.es;
-
-				}	
-			});
-		}
+		
 	},
 
 	methods: {
-	
+		initializeCalendar () {
+			this.calendarContainer = this.$el.getElementsByClassName("calendar-container")[0];
+			$(this.calendarContainer).fullCalendar(this.calendarConfig);
+			this.currentView = this.calendarConfig.defaultView;
+			this.$el.querySelector("ons-input[input-id='"+this.currentView+"']").setAttribute("checked", true);
+		},
+
+		createToolbar (){
+			let viewListButton = this.$el.querySelector("ons-toolbar-button[data-action='calendar-views']");
+			let viewsPopover = this.$el.querySelector("ons-popover[data-action='calendar-views']");
+			let filtersPopover = this.$el.querySelector("ons-popover[data-action='calendar-filters']");
+
+			let viewOptions = this.$el.querySelectorAll("ons-input[name='available-calendar-views']");
+			for (var viewOption of viewOptions) {
+				viewOption.addEventListener("change", evt => {
+
+					viewsPopover.hide();
+					let selectedView = evt.srcElement.getAttribute("id");
+					$(this.calendarContainer).fullCalendar('changeView', selectedView);
+					this.currentView = selectedView;
+
+				});
+			}
+
+			let headerLeft = this.$el.querySelector("div.fc-toolbar.fc-header-toolbar > div.fc-left");
+			let headerRight = this.$el.querySelector("div.fc-toolbar.fc-header-toolbar > div.fc-right");
+
+			let customLeftButton = document.createElement("ons-button");
+			customLeftButton.addEventListener("click", () => $(this.calendarContainer).fullCalendar('prev'));
+			let customLeftButtonIcon = document.createElement("ons-icon");
+			customLeftButtonIcon.setAttribute("icon", "fa-chevron-left");
+			customLeftButton.appendChild(customLeftButtonIcon);
+			headerLeft.appendChild(customLeftButton);
+
+			let customRightButton = document.createElement("ons-button");
+			customRightButton.addEventListener("click", () => $(this.calendarContainer).fullCalendar('next'));
+			let customRightButtonIcon = document.createElement("ons-icon");
+			customRightButtonIcon.setAttribute("icon", "fa-chevron-right");
+			customRightButton.appendChild(customRightButtonIcon);
+			headerRight.appendChild(customRightButton);
+
+			let todayButton = this.$el.querySelector("ons-toolbar-button[data-action='calendar-today']");
+
+			let filtersButton = this.$el.querySelector("ons-toolbar-button[data-action='filters-pannel']");
+
+			todayButton.addEventListener("click", () => $(this.calendarContainer).fullCalendar('today'));
+
+			viewListButton.addEventListener("click", () => viewsPopover.show(viewListButton));
+			
+			filtersButton.addEventListener("click", () => filtersPopover.show(filtersButton));
+		}
 	},
 
 	mounted() {
-		let calenndarContainer = this.$el.getElementsByClassName("calendar-container")[0];
-		$(calenndarContainer).fullCalendar(this.calendarConfig);
-		this.currentView = this.calendarConfig.defaultView;
 
-		this.$el.querySelector("ons-input[input-id='"+this.calendarConfig.defaultView+"']").setAttribute("checked", true);
-
-		let viewListButton = this.$el.querySelector("ons-toolbar-button[data-action='calendar-views']");
-		let viewsPopover = this.$el.querySelector("ons-popover[data-action='calendar-views']");
-		let filtersPopover = this.$el.querySelector("ons-popover[data-action='calendar-filters']");
-
-		let viewOptions = this.$el.querySelectorAll("ons-input[name='available-calendar-views']");
-		for (var viewOption of viewOptions) {
-			viewOption.addEventListener("change", evt => {
-
-				viewsPopover.hide();
-				let selectedView = evt.srcElement.getAttribute("id");
-				$(calenndarContainer).fullCalendar('changeView', selectedView);
-				this.currentView = selectedView;
-
-			});
-		}
-
-		let headerLeft = this.$el.querySelector("div.fc-toolbar.fc-header-toolbar > div.fc-left");
-		let headerRight = this.$el.querySelector("div.fc-toolbar.fc-header-toolbar > div.fc-right");
-
-		let customLeftButton = document.createElement("ons-button");
-		customLeftButton.addEventListener("click", () => $(calenndarContainer).fullCalendar('prev'));
-		let customLeftButtonIcon = document.createElement("ons-icon");
-		customLeftButtonIcon.setAttribute("icon", "fa-chevron-left");
-		customLeftButton.appendChild(customLeftButtonIcon);
-		headerLeft.appendChild(customLeftButton);
-
-		let customRightButton = document.createElement("ons-button");
-		customRightButton.addEventListener("click", () => $(calenndarContainer).fullCalendar('next'));
-		let customRightButtonIcon = document.createElement("ons-icon");
-		customRightButtonIcon.setAttribute("icon", "fa-chevron-right");
-		customRightButton.appendChild(customRightButtonIcon);
-		headerRight.appendChild(customRightButton);
-
-		let todayButton = this.$el.querySelector("ons-toolbar-button[data-action='calendar-today']");
-
-		let filtersButton = this.$el.querySelector("ons-toolbar-button[data-action='filters-pannel']");
-
-		todayButton.addEventListener("click", () => $(calenndarContainer).fullCalendar('today'));
-
-		viewListButton.addEventListener("click", () => viewsPopover.show(viewListButton));
+		this.initializeCalendar();
+		this.createToolbar();
 		
-		filtersButton.addEventListener("click", () => filtersPopover.show(filtersButton));
-
 	}
 }
 
