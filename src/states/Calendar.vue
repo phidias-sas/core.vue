3b4e540f773602a0abc7b98b9a9e74147878930a
Santiago Cortes <santiago.cortes@phidias.com.co>
2016-12-13 17:13:30 +0100

    toolbar in calendar state

--- src/states/Calendar.vue
+++ src/states/Calendar.vue
@@ -1,7 +1,20 @@
 <template>
-	<div>
-		<phi-full-calendar :person="app.user.id"></phi-full-calendar>
-	</div>
+
+    <div class="phi-page">
+        <div class="phi-page-cover">
+            <div class="phi-page-toolbar">
+                <button class="phi-button" @click="$parent.$el.left.toggle()"> <i class="fa fa-bars"></i></button>
+                <h1>Calendario</h1>
+            </div>            
+        </div>
+
+        <div class="phi-page-contents">
+            <div>
+                <phi-full-calendar :person="app.user.id"></phi-full-calendar>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
