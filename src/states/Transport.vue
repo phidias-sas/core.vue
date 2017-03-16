3793958537cb15096a6bdbd97b0abb417434c87a
Felipe Delgado <felipe.delgado@phidias.com.co>
2017-03-16 09:22:02 -0500

    New state Transport.vue

---
+++ src/states/Transport.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<phi-tracker
+			target	="123abc"
+			every	= 5000
+		></phi-tracker>
+	</div>
+</template>
+
+<script>
+	import PhiTracker from '../components/Phi/PhiTracker.vue';
+	import app from '../store/app.js'
+
+	export default {
+	name: 'transport',
+	components: {PhiTracker},
+		data () {
+			return {
+				app
+			}
+		},
+		mounted () {
+		}
+	}
+</script>
+
+<style lang="scss">
+
+
+</style>
