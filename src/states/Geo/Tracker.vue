94971c411e5ec0736d56e7a5fab3ba0fbb41b52f
Felipe Delgado <felipe.delgado@phidias.com.co>
2017-04-28 11:31:49 -0500

    Transport+deepstream

--- src/states/Geo/Tracker.vue
+++ src/states/Geo/Tracker.vue
@@ -1,34 +1,46 @@
 <template>
-	<div class="phi-page">
+<div class="phi-page">
+	<div class="phi-page-cover">
 		<div class="phi-page-toolbar">
-			<h1>Ubicación</h1>
-		</div>
-
-		<div class="phi-page-contents">
-			<div class="phi-card target" v-if="target">
-                <h1>{{ target.name }}</h1>
-
-                <phi-tracker :target="target.id" :every="every" :enabled="enabled" @update="update"></phi-tracker>
-
-                <select v-model="every">
-                    <option :value="3000">3 segundos</option>
-                    <option :value="10000">10 segundos</option>
-                    <option :value="20000">20 segundos</option>
-                </select>
-                <button @click="enabled = !enabled">
-                    <i v-show="enabled" class="fa fa-pause"></i>
-                    <i v-show="!enabled" class="fa fa-play"></i>
-                </button>
-
-                <ul>
-                    <li v-for="position in log">
-                        <a target="_blank" :href="`https://www.google.com.co/maps/place//@${position.latitude},${position.longitude},17z`">{{position.latitude}}, {{position.longitude}}</a>
-                    </li>
-                </ul>
-
-			</div>
+			<button class="phi-button" @click="$parent.$el.left.toggle()">
+				<i class="fa fa-bars"></i>
+			</button>
+			<h1>{{ $t('Transport') }}</h1>
 		</div>
 	</div>
+	
+	<mu-linear-progress color="#1c89b8" v-show="app.api.isLoading" />
+
+	<div class="phi-page-contents" v-if="app.user && target" >
+		<h2> {{ $t('Tracker') }} </h2>
+		<div class="phi-card target">
+			<div class="phi-media">
+				<div class="phi-media-body">
+					<h1>{{ target.name }}</h1>
+				</div>
+				<div class="phi-media-right">
+					<mu-switch :value="enabled" @change="enabled = !enabled" />	
+				</div>
+			<!--<select v-model="every">
+				<option :value="3000">3 segundos</option>
+				<option :value="10000">10 segundos</option>
+				<option :value="20000">20 segundos</option>
+			</select>-->
+			<!--<button @click="enabled = !enabled">
+				<i v-show="enabled" class="fa fa-pause"></i>
+				<i v-show="!enabled" class="fa fa-play"></i>
+			</button>-->
+			</div>
+			<!--<ul>
+				<li v-for="position in log">
+					<a target="_blank" :href="`https://www.google.com.co/maps/place//@${position.latitude},${position.longitude},17z`">{{position.latitude}}, {{position.longitude}}</a>
+				</li>
+			</ul>-->
+		</div>
+		<phi-tracker :target="target.id" :every="every" :enabled="enabled" @update="update" :targetName="target.name"></phi-tracker>
+		</div>
+	</div>
+</div>
 </template>
 
 <script>
@@ -37,44 +49,56 @@ import app from '../../store/app.js'
 
 export default {
 	name: "geo-tracker",
-    components: {PhiTracker},
+	components: {PhiTracker},
 
-    beforeRouteEnter(to, from, next) {
-        app.api
-            .get(`/geo/targets/${to.params.targetId}`)
-            .then(target => next(vm => vm.target = target));
-    },
+	beforeRouteEnter(to, from, next) {
+		app.api
+			.get(`/geo/targets/${to.params.targetId}`)
+			.then(target => next(vm => vm.target = target));
+	},
 
 	data() {
 		return {
 			app,
-            target: null,
-            every: 3000,
-            enabled: false,
-
-            log: []
+			target: null,
+			every: 3000,
+			enabled: false,
+			log: []
 		}
 	},
 
-    methods: {
-        update(position) {
-            this.log.push(position);
-        }
-    }
+	methods: {
+		update(position) {
+			this.log.push(position);
+		}
+	}
 
 }
 </script>
 
 <style lang="scss" scoped>
 .target {
-    padding: 12px;
+	padding: 12px;
 
-    h1 {
-        margin-bottom: 12px;
-    }
+	h1 {
+		margin-bottom: 0px;
+	}
 
-    img {
-        max-width: 200px;
-    }
+	img {
+		max-width: 200px;
+	}
+
+	.phi-media {
+		padding: 0px;
+	}
+}
+.phi-page-contents {
+	h2 {
+		font-weight: 1em;
+		text-transform: uppercase;
+		color: #666;
+		margin-bottom: 0.5em;
+		font-size: 0.8em;
+	}
 }
 </style>
